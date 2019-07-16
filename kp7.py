6b4047c44649b616a6eaa9d2736ac8c110ef2280
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-07-16 11:49:35 +0530

    Create kp7.py

---
+++ kp7.py
@@ -0,0 +1,8 @@
+ if (num % i) == 0: 
+           print(num, "is not a prime number") 
+           break
+   else: 
+       print(num, "is a prime number") 
+  
+else: 
+   print(num, "is not a prime number") 
